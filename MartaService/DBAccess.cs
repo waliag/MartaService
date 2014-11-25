785b22a2a8fc5085e15f9bbd598d0577993d10a2
Garima Walia <garima.nsit@gmail.com>
2014-11-25 11:58:01 -0500

    initial commit

---
+++ MartaService/DBAccess.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace MartaService
+{
+    public class DBAccess
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["TestConnection"].ConnectionString;
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public SandySpringsSchedule getSandySpringsSchedule()
+        {
+            SandySpringsSchedule timeObj;
+            timeObj = new SandySpringsSchedule();
+
+            string sql = "sp_getSandySpringsSchedule";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        DateTime currentTime = DateTime.Now;
+
+                        while (reader.Read())
+                        {
+
+                            DateTime northbound = Convert.ToDateTime(Convert.ToString(reader["NorthBound"]));
+                            DateTime southbound = Convert.ToDateTime(Convert.ToString(reader["SouthBound"]));
+                            //string southbound = Convert.ToString(reader["SouthBound"]);
+                            if ((DateTime.Compare(northbound, currentTime) > 0) &&
+                                (DateTime.Compare(southbound, currentTime) > 0))
+                            {
+                                timeObj.northBound = Convert.ToString(reader["NorthBound"]);
+                                timeObj.southBound = Convert.ToString(reader["SouthBound"]);
+                            }
+                            
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            connection.Close();
+            return timeObj;
+        }
+        public FivePointsSchedule getFivePointsSchedule()
+        {
+            FivePointsSchedule timeObj;
+            timeObj = new FivePointsSchedule();
+
+            string sql = "sp_getFivePointsSchedule";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        DateTime currentTime = DateTime.Now;
+
+                        while (reader.Read())
+                        {
+
+                            DateTime northbound = Convert.ToDateTime(Convert.ToString(reader["NorthBound"]));
+                            DateTime southbound = Convert.ToDateTime(Convert.ToString(reader["SouthBound"]));
+                            //string southbound = Convert.ToString(reader["SouthBound"]);
+                            if ((DateTime.Compare(northbound, currentTime) > 0) &&
+                                (DateTime.Compare(southbound, currentTime) > 0))
+                            {
+                                timeObj.northBound = Convert.ToString(reader["NorthBound"]);
+                                timeObj.southBound = Convert.ToString(reader["SouthBound"]);
+                            }
+
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            connection.Close();
+            return timeObj;
+        }
+    }
+}
---
+++ MartaService/IService1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MartaService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "SandySprings")]
+        [OperationContract]
+        SandySpringsSchedule GetSandySpingsSchedule();
+
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "FivePoints")]
+        [OperationContract]
+        FivePointsSchedule GetFivePointsSchedule();
+
+        // TODO: Add your service operations here
+    }
+
+
+    [DataContract]
+    public class SandySpringsSchedule
+    {
+        [DataMember]
+        public string northBound { get; set; }
+
+        [DataMember]
+        public string southBound { get; set; }
+    }
+
+    public class FivePointsSchedule
+    {
+        [DataMember]
+        public string northBound { get; set; }
+
+        [DataMember]
+        public string southBound { get; set; }
+    }
+}
